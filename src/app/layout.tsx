a0017850cc80b96cd1d067c7ce626eb4c10597e9
shagaofeng-rgb <shagaofeng@gmail.com>
2026-09-09 14:08:41 +0800

    Install Meta Pixel base code in head
    * Install Meta Pixel in document head
    
    * Send Meta events without consent delay

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import Script from "next/script";
 import { Barlow_Condensed, Manrope } from "next/font/google";
 import "./globals.css";
 import { siteConfig } from "@/config/site";
-import { ConsentAwareMetaPixel } from "@/components/analytics/meta-pixel";
 
 const displayFont = Barlow_Condensed({
   variable: "--font-display",
@@ -17,6 +16,17 @@ const bodyFont = Manrope({
   subsets: ["latin"],
   display: "swap",
 });
+
+const metaPixelCode = `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '955166104308526');
+fbq('track', 'PageView');`;
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteConfig.url),
@@ -32,9 +42,18 @@ export default function RootLayout({
         <Script id="document-locale" strategy="beforeInteractive">
           {`(function(){var l=location.pathname.split('/')[1];if(!/^(en|ar|es|pt|ja|ko)$/.test(l))l='en';document.documentElement.lang=l;document.documentElement.dir=l==='ar'?'rtl':'ltr';})();`}
         </Script>
+        <script dangerouslySetInnerHTML={{ __html: metaPixelCode }} />
       </head>
       <body className={`${displayFont.variable} ${bodyFont.variable}`}>
-        <ConsentAwareMetaPixel />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=955166104308526&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         {children}
       </body>
     </html>
