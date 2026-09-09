266df4b10627d52288b656da227ac14c3f5b97da
shagaofeng-rgb <shagaofeng@gmail.com>
2026-09-09 13:44:45 +0800

    chore: update Meta Pixel ID

--- src/components/analytics/meta-pixel.tsx
+++ src/components/analytics/meta-pixel.tsx
@@ -65,6 +65,6 @@ export function ConsentAwareMetaPixel() {
   );
   if (!enabled) return null;
   return (
-    <Script id="meta-pixel" strategy="afterInteractive">{`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1078350337870688');fbq('track','PageView');window.dispatchEvent(new Event('cowin-meta-pixel-ready'));`}</Script>
+    <Script id="meta-pixel" strategy="afterInteractive">{`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','955166104308526');fbq('track','PageView');window.dispatchEvent(new Event('cowin-meta-pixel-ready'));`}</Script>
   );
 }
